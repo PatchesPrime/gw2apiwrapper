8c89b9404e31dd103b3dddb849ff6dd4ff2cfa14
R. "Patches" S <patches@HIVE.nullcorp.org>
2017-06-04 02:38:43 -0400

    Added getRace helper.

--- __init__.py
+++ __init__.py
@@ -15,6 +15,16 @@ class GlobalAPI:
         return(getJson(self.url + api, header=None))
 
     @typer
+    def getRace(self, json):
+        '''
+        Query the non-authed Guild Wars 2 API to build
+        objects from the returned JSON..
+
+        See Race class for documentation
+        '''
+        return eps.Race(json)
+
+    @typer
     def getProfession(self, json):
         '''
         Query the non-authed Guild Wars 2 API to build
