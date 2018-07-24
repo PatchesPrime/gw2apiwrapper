e37e850f7d7b5f02ecab2fa0280e46d1fc453137
R. "Patches" S <patches@nullcorp.org>
2018-07-24 19:10:58 -0400

    Removed dependency.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email='patches@nullcorp.org',
     license='MIT',
     packages=['gw2apiwrapper'],
-    install_requires=['requests'],
+    #install_requires=['requests'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
